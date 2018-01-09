ee7588869bdb4541b8030f3e6cdae9ec8c825ade
lovexx07 <lovexx07@outlook.com>
2018-01-09 17:38:47 +0800

    彭鑫  结账页面

---
+++ app/src/main/java/com/jojo/pad/ui/activity/CheckOutctivity.java
@@ -0,0 +1,31 @@
+package com.jojo.pad.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jojo.pad.R;
+import com.jojo.pad.base.BaseAcitivty;
+
+public class CheckOutctivity extends BaseAcitivty {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_check_outctivity;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
